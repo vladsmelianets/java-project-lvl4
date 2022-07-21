f6c14caecee496c72ccf7e9b04dfc2f4f2e8d998
vlad.smelianets <vlad.smelianets@yandex.ru>
2022-07-21 18:16:44 +0300

    feat: add Hello world handler

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,11 +1,14 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
 
 public final class App {
 
     public static Javalin getApp() {
-        return Javalin.createStandalone();
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
     }
 
     public static void main(String[] args) {
