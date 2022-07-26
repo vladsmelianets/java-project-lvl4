a56104bd2d3a365386f87ed37fa1714d5a76b56f
vlad.smelianets <vlad.smelianets@yandex.ru>
2022-07-26 18:47:37 +0300

    deploy: add explicit port mapping

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,13 +5,18 @@ import io.javalin.core.JavalinConfig;
 
 public final class App {
 
+    public static void main(String[] args) {
+        getApp().start(getPort());
+    }
+
     public static Javalin getApp() {
         Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
         app.get("/", ctx -> ctx.result("Hello World"));
         return app;
     }
 
-    public static void main(String[] args) {
-        getApp().start();
+    private static int getPort() {
+        String port = System.getenv("PORT");
+        return port != null ? Integer.parseInt(port) : 8080;
     }
 }
