fba9cdf02c1e04ba252a8a2cbea6fcd180e485b2
vladsmelianets <101456442+vladsmelianets@users.noreply.github.com>
2022-12-16 10:44:28 +0300

    feat: add url validation

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -3,6 +3,7 @@ package hexlet.code.controller;
 import hexlet.code.domain.Url;
 import hexlet.code.domain.query.QUrl;
 import io.javalin.http.Handler;
+import java.net.URL;
 
 import java.util.List;
 
@@ -14,9 +15,29 @@ public final class UrlController {
     public static Handler newUrl() {
         return ctx -> {
             String name = ctx.formParam("url");
-            Url url = new Url(name);
+            
+            try {
+                URL parsed = new URL(name);
+            } catch (Exception e) {
+                ctx.sessionAttribute("flash", "Некорректный URL");
+                ctx.sessionAttribute("flash-type", "danger");
+                ctx.render("index.html");
+            }
+
 //            System.out.println("CREATING URL: " + url);
-            url.save();
+            Url url = new Url(parsed.getPath() + ":" + parsed.getPort());
+            Url existing = new QUrl()
+            .name.equalTo(url.getName())
+            .findOne();
+
+            if(existing != null) {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flash-type", "info");
+            } else {
+                url.save();
+                ctx.sessionAttribute("flash", "Страница создана");
+                ctx.sessionAttribute("flash-type", "success");
+            }            
             ctx.redirect("/urls");
         };
     }
