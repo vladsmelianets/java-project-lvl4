214323eb068d869a9d426259bd08cc89efdd8723
vladsmelianets <vlad.smelianets@yandex.ru>
2023-01-19 18:00:09 +0300

    feat: add UrlCheck model adjust Url model to reflect new model, add ebean migration files

--- app/src/main/java/hexlet/code/domain/Url.java
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -5,30 +5,29 @@ import io.ebean.annotation.WhenCreated;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.Instant;
+import java.util.List;
 
 @Entity
 public final class Url extends Model {
 
     @Id
-    private Long id;
+    private long id;
 
     private String name;
 
     @WhenCreated
     private Instant createdAt;
 
-    public Url(Long urlId, String urlName, Instant urlCreatedAt) {
-        this.id = urlId;
-        this.name = urlName;
-        this.createdAt = urlCreatedAt;
-    }
+    @OneToMany
+    private List<UrlCheck> urlChecks;
 
     public Url(String name) {
         this.name = name;
     }
 
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
